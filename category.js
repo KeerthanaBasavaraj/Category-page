31814a3d77f5f71a541d802baef72e49d0b66e62
KeerthanaBasavaraj <keerthanabasavaraj18@gmail.com>
2021-05-14 22:22:26 +0530

    Added product description

--- category.js
+++ category.js
@@ -1,90 +1,443 @@
 var AccImgObj = {
-  "images/img1.jpeg": ["covers", 100],
-  "images/head1.jpg": ["headphones", 200],
-  "images/char2.jpg": ["chargers", 300],
-  "images/head2.jpg": ["headphones", 50],
-  "images/img2.jpeg": ["covers", 60],
-  "images/img3.jpg": ["covers", 130],
-  "images/head3.jpg": ["headphones", 60],
-  "images/char3.jpg": ["chargers", 320],
-  "images/head4.jpeg": ["headphones", 546],
-  "images/img4.jpg": ["covers", 466],
-  "images/img5.jpg": ["covers", 67],
-  "images/head5.jpg": ["headphones", 77],
-  "images/head6.jpg": ["headphones", 54],
-  "images/img6.jpg": ["covers", 75],
-  "images/char4.jpg": ["chargers", 45],
-  "images/head7.jpeg": ["headphones", 67],
-  "images/img7.jpg": ["covers", 34],
-  "images/img8.jpg": ["covers", 67],
-  "images/img9.jpg": ["covers", 34],
-  "images/img10.jpeg": ["covers", 53],
-  "images/char5.jpg": ["chargers", 76],
-  "images/head13.jpg": ["headphones", 46],
-  "images/img11.jpg": ["covers", 53],
-  "images/img12.jpg": ["covers", 546],
-  "images/head12.jpg": ["headphones", 535],
-  "images/img13.jpg": ["covers", 53],
-  "images/char6.jpg": ["chargers", 46],
-  "images/head11.jpg": ["headphones", 45],
-  "images/img14.jpg": ["covers", 53],
-  "images/img15.jpg": ["covers", 65],
-  "images/head10.jpg": ["headphones", 436],
-  "images/img16.jpg": ["covers", 54],
-  "images/img17.jpg": ["covers", 76],
-  "images/head9.jpg": ["headphones", 56],
-  "images/img18.jpg": ["covers", 87],
-  "images/char1.jpg": ["chargers", 65],
-  "images/head8.jpg": ["headphones", 56],
+  "images/img1.jpeg": [
+    "covers",
+    100,
+    "Iphone 11 cover",
+    "Pidgeot Transparent Bumper Back (6.1 inch) Case",
+  ],
+  "images/head1.jpg": [
+    "headphones",
+    200,
+    "YAMAY Bluetooth Headphones",
+    "Earphones come with Bluetooth version 5.0 with a range of 10 metres",
+  ],
+  "images/char2.jpg": [
+    "chargers",
+    300,
+    "Android charger",
+    "Portable and sleek BoxCod Quick Charge 3.0 USB",
+  ],
+  "images/head2.jpg": [
+    "headphones",
+    50,
+    "Microsoft surface headphones",
+    "Wireless Noise Cancelling Over-The-Ear with Cortana - Light Gray",
+  ],
+  "images/img2.jpeg": [
+    "covers",
+    60,
+    "Iphone mobile phone case",
+    "Gripp Dazzle Back Cover Case For Apple Iphone 11 Pro (Gold)",
+  ],
+  "images/img3.jpg": [
+    "covers",
+    130,
+    "OPPOA37 mobile phone case",
+    "Silicone anti-drop a37m soft shell a37t protective cover",
+  ],
+  "images/head3.jpg": [
+    "headphones",
+    60,
+    "Pink wireless headphones",
+    "Noise Shots XO Wireless Bluetooth Earbuds with Great Call Quality",
+  ],
+  "images/char3.jpg": [
+    "chargers",
+    320,
+    "USB-C",
+    "24-pin USB connector system with a rotationally symmetrical connector",
+  ],
+  "images/head4.jpeg": [
+    "headphones",
+    546,
+    "Montblanc headphones",
+    "Comfortable companions for longer meeting days and sound quality",
+  ],
+  "images/img4.jpg": [
+    "covers",
+    466,
+    "Iphone 12 phone case aesthetic",
+    "Retro Collage Compatible with iPhone 6 6s 7 8 X Xs Xr 11 12 Pro Max",
+  ],
+  "images/img5.jpg": [
+    "covers",
+    67,
+    "Aesthetic case for gold iphone",
+    "Casery Black & Gold Agate",
+  ],
+  "images/head5.jpg": [
+    "headphones",
+    77,
+    "Mode eq marshall",
+    "Offers huge sound in a small package",
+  ],
+  "images/head6.jpg": [
+    "headphones",
+    54,
+    "Dell wireless headphones",
+    "YTF86 USB Headphones (Black)",
+  ],
+  "images/img6.jpg": [
+    "covers",
+    75,
+    "Pixel 3axl wallet case",
+    "CH-IC PU Leather Folding Flip Shockproof Protective Cell Shell",
+  ],
+  "images/char4.jpg": [
+    "chargers",
+    45,
+    "Original iphone 7 plus charger",
+    "Wall Charger With Data Sync And Cable for Apple iPhone",
+  ],
+  "images/head7.jpeg": [
+    "headphones",
+    67,
+    "Oppo Vivo Wireless Headphone",
+    "Sweatproof Neckband Bluetooth Headset (Black)",
+  ],
+  "images/img7.jpg": [
+    "covers",
+    34,
+    "Google pixel 4a flip case",
+    "Protection with a kickstand. Size: 5.9 x 3 x 0.49 inches | Weight: 1.7 ounces",
+  ],
+  "images/img8.jpg": [
+    "covers",
+    67,
+    "Kowauri Google Pixel 4a Case",
+    "Leather Wallet Case Classic Design",
+  ],
+  "images/img9.jpg": [
+    "covers",
+    34,
+    "Iphone xr phone cases",
+    "Light Blue Liquid Silicone Case with Soft Microfiber Lining",
+  ],
+  "images/img10.jpeg": [
+    "covers",
+    53,
+    "Iphone x blue phone case",
+    "Compatible Devices : apple; Model : i phone x; Material Type : Silicone",
+  ],
+  "images/char5.jpg": [
+    "chargers",
+    76,
+    "Gji phantom professional charger",
+    "Various colors stereo earphone for mp3/mp4, CD, PC/laptop& mobile",
+  ],
+  "images/head13.jpg": [
+    "headphones",
+    46,
+    " Shensen Headphones",
+    "Various colors stereo earphone for mp3/mp4, CD, PC/laptop",
+  ],
+  "images/img11.jpg": [
+    "covers",
+    53,
+    "Totallee iPhone 11 Pro Leather Case",
+    " Ultra Slim & Thin Premium Leather Luxury, Silicone Phone Case",
+  ],
+  "images/img12.jpg": [
+    "covers",
+    546,
+    "Oppo a31 cover",
+    "Multicolor lord mahadev shiva shiv ji mahakal back cover case",
+  ],
+  "images/head12.jpg": [
+    "headphones",
+    535,
+    "Helena Krausson (albio52)",
+    "Ornate headphones that are bedazzled with glamorous stones",
+  ],
+  "images/img13.jpg": [
+    "covers",
+    53,
+    "Samsung Galaxy A71 Phone Case(Orange)",
+    "Glitter Sparkle Bling Luxury Case",
+  ],
+  "images/char6.jpg": [
+    "chargers",
+    46,
+    "Typo marble wireless portable charger",
+    "It comes with travel pouch, micro USB cable, and 18-month warranty",
+  ],
+  "images/head11.jpg": [
+    "headphones",
+    45,
+    "KEEKA KE 900 Macaroon Headphone",
+    "Noise Cancelling fashionable headphone",
+  ],
+  "images/img14.jpg": [
+    "covers",
+    53,
+    "Samsung Galaxy A71 Phone Case(purple)",
+    "Glitter Sparkle Bling Luxury Case",
+  ],
+  "images/img15.jpg": [
+    "covers",
+    65,
+    "Case for Realme C3",
+    "Use this Flowery Case to protect your phone from scratches",
+  ],
+  "images/head10.jpg": [
+    "headphones",
+    436,
+    "BaubleBar Helios Headphone",
+    "Stylish headphone with noice cancelling",
+  ],
+  "images/img16.jpg": [
+    "covers",
+    54,
+    "Lspcase Samsung Galaxy Tab cover",
+    "10.1 Inch SM-T510 / SM-T515 (2019) Case PU Leather Flip Cover",
+  ],
+  "images/img17.jpg": [
+    "covers",
+    76,
+    "Iphone 12 mini screen protector tempered glass",
+    "Lithium Aluminosilicate provides upgraded strength and flexibility",
+  ],
+  "images/head9.jpg": [
+    "headphones",
+    56,
+    "MDI Tone+ Bluetooth earphone",
+    "One touch incoming call receive functions from up to 10 feet distance",
+  ],
+  "images/img18.jpg": [
+    "covers",
+    87,
+    "Xiaomi redmi 9 cover",
+    "3d silicone fruit case",
+  ],
+  "images/char1.jpg": [
+    "chargers",
+    65,
+    "Samsung cell charger",
+    "Type C Cable Fast USB C Charging 6FT 3Pack Power Cord",
+  ],
+  "images/head8.jpg": [
+    "headphones",
+    56,
+    "Beats Solo 3(Rose gold)",
+    "Fast Fuel, 5 minutes of charging gets you 3 hours of playback",
+  ],
 };
 
 const accImgArray = Object.keys(AccImgObj);
 const accAttriArray = Object.values(AccImgObj);
 
 var EleImgObj = {
-  "images/tv1.jpg": ["television", 3000],
-  "images/desk1.jpg": ["desktop", 4000],
-  "images/pho1.jpg": ["mobile", 1000],
-  "images/tv2.jpg": ["television", 4000],
-  "images/tv3.jpg": ["television", 3990],
-  "images/lap1.jpg": ["laptop", 3300],
-  "images/pho2.jpg": ["mobile", 2200],
-  "images/ref3.jpg": ["refrigerator", 6000],
-  "images/ref1.jpg": ["refrigerator", 6000],
-  "images/lap2.jpg": ["laptop", 3300],
-  "images/desk2.jpg": ["desktop", 4000],
-  "images/desk3.jpg": ["desktop", 4000],
-  "images/ref2.jpg": ["refrigerator", 6000],
-  "images/tv4.jpg": ["television", 4000],
-  "images/desk3.jpg": ["desktop", 4000],
-  "images/pho3.jpg": ["mobile", 2200],
-  "images/tv5.jpeg": ["television", 3990],
-  "images/lap3.jpg": ["laptop", 3300],
-  "images/desk4.jpg": ["desktop", 4000],
-  "images/ref4.jpg": ["refrigerator", 6000],
+  "images/tv1.jpg": [
+    "television",
+    3000,
+    "UHD 4K Curved Smart TV ",
+    "A powerful processor optimizes your TV's performance with 4K picture quality",
+  ],
+  "images/desk1.jpg": [
+    "desktop",
+    4000,
+    "HP Desktop Core 2 Duo",
+    "Hard Drive Capacity: 1TB, 320GB , 500GB. Condition: New. Processor: i3, i5, i7. Internal Memory (ROM): 4 GB",
+  ],
+  "images/pho1.jpg": [
+    "mobile",
+    1000,
+    "Apple iPhone 12 Pro Max",
+    "6.7' display, Apple A14 Bionic chipset, 3687 mAh battery, 512 GB storage",
+  ],
+  "images/tv2.jpg": [
+    "television",
+    4000,
+    "Realme smart android tv",
+    "Premium Bezel-less design with bezels, Stunning Picture Quality and Powerful Processor",
+  ],
+  "images/tv3.jpg": [
+    "television",
+    3990,
+    "LG 24' LED TV",
+    "Screen size - 24'|Screen technology - PICTURE QUALITY| Motion rate - 50 Hz",
+  ],
+  "images/lap1.jpg": [
+    "laptop",
+    3300,
+    "Lenovo ip slim 3i",
+    "39.62cms, Platinum Grey|Model Number : 81WB00ANIN",
+  ],
+  "images/ref3.jpg": [
+    "refrigerator",
+    6000,
+    "Kimchi refrigerator",
+    "It has a massive screen that runs apps, browses the web, monitors your groceries, and much more",
+  ],
+  "images/ref1.jpg": [
+    "refrigerator",
+    6000,
+    "Samsung refrigerator",
+    "260 litres capacity that can easily work out single or multiple people",
+  ],
+  "images/lap2.jpg": [
+    "laptop",
+    3300,
+    "HP envy 13",
+    "i7 processor; Windows 10 Home Single Language 64",
+  ],
+  "images/desk2.jpg": [
+    "desktop",
+    4000,
+    "IMac Apple",
+    "M1 chip comes with configurable memory, storage, and more. Available in 7 colors",
+  ],
+  "images/desk3.jpg": [
+    "desktop",
+    4000,
+    "Imac noir",
+    "Retina 5K Display Z0VT-MRR12-20 B&H",
+  ],
+  "images/ref2.jpg": [
+    "refrigerator",
+    6000,
+    "Samsung Food Showcase",
+    "RF28R6301SR French Door with Twin Cooling",
+  ],
+  "images/tv4.jpg": [
+    "television",
+    4100,
+    "LG 4K UHD Smart LED TV",
+    "Stunning Picture Quality and Powerful Processor",
+  ],
+  "images/desk3.jpg": ["desktop", 4000, "Noir - iMac", "Retina 5K, 27 pouces"],
+  "images/pho3.jpg": [
+    "mobile",
+    2200,
+    "LAVA Z92",
+    "3 GB RAM, 32 GB ROM, 3260 mAh battery, 13 MP back camera",
+  ],
+  "images/tv5.jpeg": [
+    "television",
+    3990,
+    "Realme smart tv 32",
+    "80 cm (32 inch) HD Ready LED Smart Android TV",
+  ],
+  "images/lap3.jpg": [
+    "laptop",
+    3300,
+    "Mac Apple",
+    "Processor-i7 | Memory Size (RAM) - 4 GB | Internal Memory (ROM) -	500 GB",
+  ],
+  "images/desk4.jpg": [
+    "desktop",
+    4000,
+    "Lenovo ThinkCenter Desktop",
+    "6-Core, 12MB Cache, 3.1GHz to 4.5GHz, 65W, 2.5 inch 1TB 7200rpm SATA Hard Disk Drive",
+  ],
+  "images/ref4.jpg": [
+    "refrigerator",
+    6000,
+    "Hitachi Refrigerator Bottom Freezer",
+    "Large-capacity single door, smart and extendable compartment for fresh vegetables",
+  ],
 };
 
 const eleImgArray = Object.keys(EleImgObj);
 const eleAttriArray = Object.values(EleImgObj);
 
 var EntImgObj = {
-  "images/inst1.jpg": ["instrument", 600],
-  "images/book2.jpg": ["book", 110],
-  "images/toy2.jpg": ["toy", 150],
-  "images/book7.jpg": ["book", 100],
-  "images/inst4.jpg": ["instrument", 500],
-  "images/toy1.jpeg": ["toy", 100],
-  "images/book3.jpg": ["book", 100],
-  "images/inst2.jpg": ["instrument", 600],
-  "images/toy3.jpg": ["toy", 250],
-  "images/book4.jpg": ["book", 200],
-  "images/book5.jpeg": ["book", 160],
-  "images/toy4.jpg": ["toy", 250],
-
-  "images/book6.jpg": ["book", 290],
-  "images/inst3.jpg": ["instrument", 900],
-  "images/book1.jpg": ["book", 300],
-  "images/inst5.jpg": ["instrument", 680],
+  "images/inst1.jpg": [
+    "instrument",
+    600,
+    "Banjira Pro Tabla",
+    "Nickel Plated Brass Bayan and 5.50' Dayan",
+  ],
+  "images/book2.jpg": [
+    "book",
+    110,
+    "A Game of Thrones",
+    "Set of 6 Books, English, Paperback, Martin George R.R.",
+  ],
+  "images/toy4.jpg": [
+    "toy",
+    250,
+    "Kids Car",
+    "Suitable for Boys & Girls(1-3 Years)",
+  ],
+  "images/toy2.jpg": [
+    "toy",
+    150,
+    "Candywood kid's phone",
+    "Suitable for kids(1-4 Years)",
+  ],
+  "images/book7.jpg": [
+    "book",
+    100,
+    "2 States",
+    "This fun-filled love story that gets complicated when the question of marriage comes up",
+  ],
+  "images/inst4.jpg": [
+    "instrument",
+    500,
+    "Asago 4 String Guitar",
+    "Good sound quality and good wooden finishing",
+  ],
+  "images/toy1.jpeg": [
+    "toy",
+    100,
+    "Little's Junior Stacking Ring Toys",
+    "Plastic Material | Multicolor | Age: 6+ Months",
+  ],
+  "images/book3.jpg": [
+    "book",
+    100,
+    "Famous Five",
+    "Paperback by Enid Blyton, Dimensions: 19.5 x 13 x 4.3cm",
+  ],
+  "images/inst2.jpg": [
+    "instrument",
+    600,
+    "Eucalyptus Wooden Veena",
+    "Made of eucalyptus wood and non-toxic material, Dimension: (L x B x H): 5 x 5 x 16 cm Brown",
+  ],
+  "images/toy3.jpg": [
+    "toy",
+    250,
+    "Noodley Rainbow Teddy Bear",
+    "16 inch LED Stuffed Light-Up Toy",
+  ],
+  "images/book4.jpg": [
+    "book",
+    200,
+    "The Little Prince",
+    "Language : English, Item Weight : 850 g, Dimensions : 24 x 18 x 1 cm",
+  ],
+  "images/book5.jpeg": [
+    "book",
+    160,
+    "World Famous Adventure Stories",
+    "Paperback, Manoj Publications Editorial Board",
+  ],
+  "images/book6.jpg": [
+    "book",
+    290,
+    "A Brief History of Time",
+    "This fully updated edition is enriched with a new Introduction by Professor Hawking",
+  ],
+  "images/inst3.jpg": [
+    "instrument",
+    900,
+    "Saraswati Veena",
+    "Material: Wooden, Color: Brown, No Of Strings: 10",
+  ],
+  "images/book1.jpg": [
+    "book",
+    300,
+    "Best of Children’s Classics",
+    "Set of 5 Books",
+  ],
+  "images/inst5.jpg": [
+    "instrument",
+    680,
+    "SG Musical Bamboo Flute",
+    "Fine Quality, Bamboo Flute,  G Scale",
+  ],
 };
 
 const entImgArray = Object.keys(EntImgObj);
@@ -98,7 +451,10 @@ function createContainer(i, imgArray, attriArray, container) {
   const parentDiv = document.createElement("div");
   const figure = document.createElement("img");
   const childDiv = document.createElement("div");
+  const productDesc = document.createElement("p");
+  const productName = document.createElement("p");
   const priceTag = document.createElement("p");
+  const cartButton = document.createElement("button");
 
   container.appendChild(parentDiv);
   parentDiv.classList.add(
@@ -116,13 +472,21 @@ function createContainer(i, imgArray, attriArray, container) {
   figure.alt = attriArray[i][0];
   figure.classList.add("img-responsive");
   priceTag.innerText = "$" + attriArray[i][1];
+  productName.innerText = attriArray[i][2];
+  productDesc.innerText = attriArray[i][3];
+  cartButton.innerText = "Add to Cart";
   childDiv.classList.add("card-body");
-  priceTag.classList.add("img-price" ,"card-text");
+  priceTag.classList.add("img-price", "card-text");
+  cartButton.classList.add("btn", "btn-primary", "btn-style");
+  productName.classList.add("productName");
+  productDesc.classList.add("productDesc");
 
   parentDiv.appendChild(figure);
   parentDiv.appendChild(childDiv);
+  childDiv.appendChild(productName);
+  childDiv.appendChild(productDesc);
   childDiv.appendChild(priceTag);
-
+  childDiv.appendChild(cartButton);
 }
 
 for (var i = 0; i < accImgArray.length; i++) {
@@ -158,24 +522,23 @@ var b = localStorage.getItem("myValue");
 console.log(b);
 
 function responsiveAlignment(elemId) {
-  if (window.innerWidth < 610){
+  if (window.innerWidth < 610) {
     document.getElementById(elemId).style.display = "inline";
-  }
-  else {
+  } else {
     document.getElementById(elemId).style.display = "flex";
     document.getElementById(elemId).style.flexDirection = "row-reverse";
   }
-};
+}
 
 if (b == 2) {
-    responsiveAlignment("elec");
-    window.addEventListener('resize', responsiveAlignment("elec"));
+  responsiveAlignment("elec");
+  window.addEventListener("resize", responsiveAlignment("elec"));
 } else if (b == 3) {
   responsiveAlignment("enter");
-  window.addEventListener('resize', responsiveAlignment("enter"));
+  window.addEventListener("resize", responsiveAlignment("enter"));
 } else {
   responsiveAlignment("acce");
-  window.addEventListener('resize', responsiveAlignment("acce"));
+  window.addEventListener("resize", responsiveAlignment("acce"));
 }
 
 $(document).ready(function () {
